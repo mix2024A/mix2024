e83c0dd61aadb223d0a347a013ff0a211ef711ac
mix2024 <tmdrn8455@example.com>
2024-08-09 17:54:15 +0900

    Staging changes before rebase

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ app.get('/search', (req, res) => {
 
 
 const PORT = process.env.PORT || 8080;
->>>>>>> 8f14ed8 (Your commit message)
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
