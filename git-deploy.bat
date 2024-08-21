5a1cebc8ae0b9b0240c4ea1615c524e653a7a169
mix2024A <tmdrn8455@gmail.com>
2024-08-22 01:18:55 +0900

    Auto commit message

--- git-deploy.bat
+++ git-deploy.bat
@@ -10,7 +10,8 @@ REM 원격 저장소에 푸시
 git push origin main
 
 REM SSH를 통해 원격 서버에 접속하여 Git 명령어 실행
-ssh -i "C:\Users\pok_m\.ssh\mix2024.pem" ubuntu@3.36.63.212 "cd ~/mix2024 && git stash && git pull origin main && git stash pop && pm2 restart server"
+ssh -i "C:\Users\pok_m\.ssh\mix2024.pem" ubuntu@3.36.63.212 ^
+"cd ~/mix2024 && git stash && git pull origin main && git stash pop && pm2 restart server"
 
 REM 배포 스크립트 종료
 exit
