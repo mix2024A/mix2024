f2f9cb1af850fad1667295bff7bede689f073ba5
mix2024A <tmdrn8455@gmail.com>
2024-08-22 01:30:24 +0900

    Your commit message

--- git-deploy.bat
+++ git-deploy.bat
@@ -2,15 +2,19 @@
 
 REM 로컬 저장소의 모든 변경 사항을 스테이지에 추가
 git add .
+if errorlevel 1 exit /b %errorlevel%
 
 REM 커밋 메시지 입력 (원하는 커밋 메시지로 변경)
 git commit -m "Your commit message"
+if errorlevel 1 exit /b %errorlevel%
 
 REM 원격 저장소에 변경 사항 푸시
 git push origin main
+if errorlevel 1 exit /b %errorlevel%
 
 REM SSH를 통해 원격 서버에 접속하여 Git 명령어 실행
 ssh -i "C:\Users\pok_m\.ssh\mix2024.pem" ubuntu@3.36.63.212 "cd ~/mix2024 && git stash && git pull origin main && git stash pop && pm2 restart server"
+if errorlevel 1 exit /b %errorlevel%
 
 REM 배포 스크립트 종료
 exit
