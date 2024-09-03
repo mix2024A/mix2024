69d6324c66c67dbefa7e9da81f03cced501bd79f
mix2024A <tmdrn8455@gmail.com>
2024-09-03 14:46:00 +0900

    Your commit message

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,7 +34,7 @@ exports.getUserInfo = (req, res) => {
 exports.getDeletedKeywords = (req, res) => {
     validateSession(req, res, () => {
         const query = `
-            SELECT search_term, display_keyword, slot, created_at, deleted_at, note, ranking
+            ranking, SELECT search_term, display_keyword, slot, created_at, deleted_at, note
             FROM deleted_keywords
             WHERE username = ?  -- 현재 로그인한 사용자와 연관된 키워드만 가져옴
             ORDER BY deleted_at DESC
